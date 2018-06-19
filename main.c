45921a422a62dff119152c7501ae8d6e986279b8
Gilly Barr <gillyb@google.com>
2018-06-19 10:27:30 +0300

    Progress

--- main.c
+++ main.c
@@ -7,9 +7,10 @@
 // TODO: These should come from the user input
 // const int PAGE_SIZE = 40;   // in bytes
 // const int NUM_PAGES = 10;
-
 // const int HASH_MOD = 5;
 
+const char* dir = "./results/";
+int c = 0;
 
 // Structure that defines a row in the input table
 typedef struct TableRow {
@@ -17,70 +18,148 @@ typedef struct TableRow {
     int fieldValue;
 } TableRow;
 
-int getHashValue(int value) {
-    int p = HASH_MOD;
-    return value % p;
+void writePageToFile(char* filename, TableRow* memoryPage, int pageSize) {
+    FILE* pageFile = fopen(filename, "a");
+    // int memoryPageSize = sizeof(memoryPage) / sizeof(TableRow);
+    fprintf(pageFile, "\n");
+    for (int i=0; i < 3; i++) {
+        if (memoryPage[i].fieldId == 0 && memoryPage[i].fieldValue == 0) {
+            fprintf(pageFile, "...\n");
+            continue;
+        }
+        // printf(".");
+        fprintf(pageFile, "%d %d\n", memoryPage[i].fieldId, memoryPage[i].fieldValue);
+    }
+    c++;
+    fclose(pageFile);
 }
 
-void writePageToFile(char* filename, TableRow memoryPage[], int pageSize) {
-    FILE *f = fopen(filename, "w");
-    for (int i=0; i<pageSize; i++) {
-        fprintf(f, "%d %d\n", memoryPage[i].fieldId, memoryPage[i].fieldValue);
+int c2 = 0;
+void saveAndClearBucket(int hashValue, TableRow** buckets, int numRowsInPage) {
+    char hashFilename[100];
+    char hashValueStr[5];
+
+    sprintf(hashValueStr, "%d", hashValue);
+    strcpy(hashFilename, dir);
+    strcat(hashFilename, hashValueStr);
+
+    printf("saving %d rows to hash file %d", numRowsInPage, hashValue);
+    c2 += numRowsInPage;
+
+    writePageToFile(hashFilename, buckets[hashValue], numRowsInPage);
+
+    // memset(bucket, '\0', sizeof(bucket));
+    for (int i=0; i<numRowsInPage; i++) {
+        buckets[hashValue][i].fieldId = 0;
+        buckets[hashValue][i].fieldValue = 0;
     }
-    fclose(f);
+}
+
+void getUserInput(int* pageSizeBytes, int* numPagesInBuffer, int* hashMod) {
+    // TODO: Check the limits of these (according to page 137)
+    printf("We are going to produce a hash-join.\nFirst we need some information..\n\n");
+    printf("What is the system's page size (in bytes) ?");
+    scanf("%d", pageSizeBytes);
+    printf("\nHow many pages are in each buffer ?");
+    scanf("%d", numPagesInBuffer);
+    printf("\nHow many buckets should we hash ?");
+    scanf("%d", hashMod);
+
+    printf("\npageSizeBytes: %d\n", *pageSizeBytes);
+    printf("numPagesInBuffer: %d\n", *numPagesInBuffer);
+    printf("hashMod: %d\n\n", *hashMod);
 }
 
 int main(int argc, char *argv[]) {
 
     // input parameters
     int pageSizeBytes, numPagesInBuffer, hashMod;
+    // getUserInput(&pageSizeBytes, &numPagesInBuffer, &hashMod);
+    pageSizeBytes = 24;
+    numPagesInBuffer = 12;
+    hashMod = 4;
 
-    // Get user input
-    printf("We are going to produce a hash-join.\nFirst we need some information..\n\n");
-    printf("What is the system's page size (in bytes) ?");
-    scanf("%d", &pageSizeBytes);
-    printf("\nHow many pages are in each buffer ?");
-    scanf("%d", &numPagesInBuffer);
-    printf("\nHow many buckets should we hash ?");
-    scanf("%d", &hashMod);
-
-    // Say hello, and print command line arguments
-    // printf("hello world\n");
-    // for (int i=0; i<argc; i++) {
-        // printf("arg %d: %s\n", i, argv[i]);
-    // }
-    
     // Calculate how many rows fit in one page
-    int numRowsInPage = pageSize / (sizeof(int) + sizeof(int));
+    int numRowsInPage = pageSizeBytes / sizeof(TableRow);
+    // printf("\nEach row takes %d bytes\n", sizeof(TableRow));
     printf("We can fit %d table rows in each memory page.\n", numRowsInPage);
 
-    // Create memory page structure
-    TableRow memoryPage[numRowsInPage];
 
-    // read datafile
+
+
+
+    // Create hash buckets
+    // We create 'hashMod' buckets (This is the amount of buckets from the user input)
+    // Each bucket has 'numRowsInPage' table rows (This is the amount of rows that can fit in 1 memory page)
+    // TableRow hashBuckets[hashMod][numRowsInPage];
+    // TableRow hashBuckets[hashMod][4];
+
+    TableRow **hashBuckets = (TableRow **)malloc(hashMod * sizeof(TableRow *));
+    for (int i = 0; i < hashMod; i++)
+        hashBuckets[i] = (TableRow *)malloc(numRowsInPage * sizeof(TableRow));
+
+    int hashBucketsCount[hashMod];
+    for (int i=0; i<hashMod; i++) hashBucketsCount[i] = 0;
+    // memset(hashBucketsCount, 0, hashMod);
+
+    // start reading table from datafile
     int i=0;
-    char *filename = "D:\\OpenU\\DB Implementation\\datafile.txt";
-    FILE *fp = fopen(filename, "r"); // open in read only mode
-    char fileLine[256];
+    char *filename = "./datafile.txt";  // TODO: get this from the user
+    FILE *fp = fopen(filename, "r");    // open in read only mode
+
+    int fieldSize = 5;
+    int lineSize = sizeof(char) * (fieldSize + fieldSize + 3);
+
+    char fileLine[lineSize];        // We can assume that each line in the file is smaller than a memory page
     while (fgets(fileLine, sizeof(fileLine), fp)) {
-        // If line is longer than 256 characters, we need to read more.
-        printf("%s", fileLine);
+
         int fieldSize = 5;
 
         char* field = malloc(fieldSize * sizeof(char));
-        strncpy(field, fileLine, fieldSize);
         char* value = malloc(fieldSize * sizeof(char));
-        strncpy(value, fileLine + fieldSize + 1, 5);
 
-        if (i < numRowsInPage) {
-            memoryPage[i].fieldId = atoi(field);
-            memoryPage[i].fieldValue = atoi(value);
+        strncpy(field, fileLine, fieldSize);
+        strncpy(value, fileLine + 1 + fieldSize, fieldSize);
+
+
+        int hashValue = atoi(field) % hashMod;      // Get the hash for this id
+        // printf(":%d", hashValue);
+
+        int bucketCounter = hashBucketsCount[hashValue];
+        // printf("  hashValue: %d\n", hashValue);
+        hashBuckets[hashValue][bucketCounter].fieldId = atoi(field);
+        hashBuckets[hashValue][bucketCounter].fieldValue = atoi(value);
+
+        // printf("  ->: %d\n", hashValue);
+
+        hashBucketsCount[hashValue] = hashBucketsCount[hashValue] + 1;
+        if (hashBucketsCount[hashValue] == numRowsInPage) {
+            saveAndClearBucket(hashValue, hashBuckets, numRowsInPage);
+            hashBucketsCount[hashValue] = 0;
         }
 
+        // free(field);
+        // free(value);
+        printf("i: %d, ", i);
         i++;
     }
 
-    writePageToFile("gilly.txt", memoryPage, numRowsInPage);
+    printf("\ntotal count: %d", i);
+    // Save the hash buckets that are left
+    for (int i=0; i<hashMod; i++) {
+        saveAndClearBucket(i, hashBuckets, numRowsInPage);
+    }
+
+    // test
+    for (int i=0; i<hashMod; i++) {
+        printf("\n\nbucket %d: %d rows\n", i, hashBucketsCount[i]);
+    }
+
+    printf("\n\nall done :)");
+    printf("\nwrote %d pages", c);
+    printf("\nagain, wrote %d rows", c2);
+
+    // writePageToFile("gilly.txt", memoryPage, numRowsInPage);
 
     fclose(fp);
     return 0;
